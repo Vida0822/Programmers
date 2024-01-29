33eaaad6c49705d995957413ecdb49b5cee13e4c
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2024-01-29 16:59:07 +0900

    [level 3] Title: 단속카메라, Time: 21.61 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -3,44 +3,45 @@ class Solution {
     public int solution(int[][] routes) {
         /*
         사용 로직 
-        : 이어져있지 않은 부분을 기준으로 그룹을 나눠 각각 감시 카메라를 설치한다 
-        1) 각 차를 순서대로 고려 
-        2) 차의 진입지점이 그룹의 최대 진출지점보다 더 갔을 때 new 그룹 
-        3) 차의 진출지점이 그룹의 최소 진입지점보다 덜 갔을 때 new 그룹 
-        4) 그룹이 겹칠 땐 최소 진입지점, 최대 진출지점 갱신 
-        5) 그룹 갯수만큼 감시 카메라 설치 
+        : 진출/진입 가로 그래프가 이어져있지 않은 부분을 기준으로 그룹을 나눠 각각 감시 카메라를 설치한다
+        <-> 가장 먼저 나가는 차량부터 진출 지점을 기준으로 카메라를 설치한 후, 이후에 나가는 차량들을 검사
+ 
         */
         
+        // 1. 차들을 '나간지점'을 기준으로 오름차순 
+        // 	[-18, -16], [-20, -15], [-14, -5], [-5, -3]
         ArrayList<Car> list = new ArrayList<>() ; 
         for(int i = 0 ; i < routes.length ; i++){
             Car car = new Car(routes[i][0], routes[i][1]) ; 
             list.add(car) ; 
         } // for 
-        
-        // 2. 
         Collections.sort(list, (a,b) -> {  // 비교하는 두 Car 
             return Integer.compare(a.out, b.out) ; // 진출 시점으로 정렬 
         }) ; 
             
-        // 3. 
+        // 2. 감시 카메라 설치 --> 해당 감시카메라로 찍을 수 있는 차 확인 
         boolean[] included = new boolean[list.size()] ; 
         // 해당 차가 감시카메라 영역에 포함되었는지 여부 
         
-        int group = 0 ; 
+        int camera = 0 ; 
         for(int i = 0 ; i < list.size() ; i++){
             if(included[i]) 
                 continue ; 
             
-            group++ ; 
-            // 해당 그룹에 포함되는 차들 검사 
+            camera++ ; // 감시 카메라에 포함되지 않으면, 새로운 감시카메라 설치
+            // 해당 감시 카메라 영역에 포함되는 차들 검사 
             for(int j = i+1 ; j < list.size(); j++){
+                // 검사 대상 차들은 감시카메라 지점보다 더 늦게 나감  
                 if(list.get(i).out >= list.get(j).in){
-                    // 해당 그룹의 최대 진출 시점 이전에 차의 진입 시점이 우선하면 
-                    included[j] = true ; // 해당 차는 감시카메라에 속함 
+                    // 진출 시점이 감시카메라 설치 위치보다 이전이면  
+                    included[j] = true ; // 해당 차는 감시카메라 지점보다 이전에 들어오고, 이후에 나가는 것 ==> 해당 감시카메라 영역에 포함
                 } // if 
             } // j             
         } // i
-        return group ; 
+        
+        // 3. 최종적인 감시카메라 갯수 반환 
+        return camera ; 
+        
     } // solution 
 } // class 
 
