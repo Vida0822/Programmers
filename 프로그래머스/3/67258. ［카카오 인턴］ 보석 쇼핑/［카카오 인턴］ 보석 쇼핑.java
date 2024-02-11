dcc21a956cfde89211014742b3cb31622e8e33a5
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2024-02-11 21:31:09 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 76.41 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,77 +1,26 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-public class Solution {
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(new Solution().solution(
-                new String[]{"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"})));
-    }
-
+import java.util.*;
+class Solution {
     public int[] solution(String[] gems) {
-        int[] answer = {};
+        int kind = new HashSet<>(Arrays.asList(gems)).size();
 
-        // 보석 종류
-        Set<String> gemSet = new HashSet<>(List.of(gems));
-        answer = findTerm(gemSet, gems);
+        int[] answer = new int[2];
+        int length = Integer.MAX_VALUE, L = 0;
+        Map<String, Integer> map = new HashMap<>();
+        for (int R = 0; R < gems.length; R++) {
+            map.put(gems[R], map.getOrDefault(gems[R], 0) + 1);
 
-        return answer;
-    }
+            while (map.get(gems[L]) > 1) {
+                map.put(gems[L], map.get(gems[L]) - 1);
+                L++;
+            }
 
-    public int[] findTerm(Set<String> gameSet, String[] gems) {
-        int l = 0;
-        int r = 0;
-        Node minNode = new Node(l, r, Integer.MAX_VALUE);
-
-        Map<String, Integer> list = new HashMap<>();
-        while (l != gems.length) {
-            if (list.size() == gameSet.size()) { // 다 찾은 경우
-                if (minNode.distance >= Math.abs(l - r)) {
-                    if (minNode.distance == Math.abs(l - r) && minNode.l > l) {
-                        minNode = new Node(l, r - 1);
-                    } else {
-                        minNode = new Node(l, r - 1);
-                    }
-                }
-
-                if (list.get(gems[l]) - 1 == 0) {
-                    list.remove(gems[l]);
-                } else {
-                    list.put(gems[l], list.get(gems[l]) - 1);
-                }
-
-                l++;
-            } else { // 다 못찾은 경우
-                if (r < gems.length) {
-                    list.put(gems[r], list.getOrDefault(gems[r], 0) + 1);
-                    r++;
-                } else {
-                    break;
-                }
+            if (map.size() == kind && length > (R - L)) {
+                length = R - L;
+                answer[0] = L + 1;
+                answer[1] = R + 1;
             }
         }
 
-        return new int[]{minNode.l + 1, minNode.r + 1};
-    }
-
-    class Node {
-        int l;
-        int r;
-        int distance;
-
-        Node(int l, int r, int distance) {
-            this.l = l;
-            this.r = r;
-            this.distance = distance;
-        }
-
-        Node(int l, int r) {
-            this.l = l;
-            this.r = r;
-            this.distance = Math.abs(l - r);
-        }
+        return answer;
     }
 }
