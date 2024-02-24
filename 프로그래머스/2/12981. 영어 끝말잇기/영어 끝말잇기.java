5b0ae7bbcc399b76977a0f2e71a408eb990750c9
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2024-02-24 10:18:02 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.05 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,22 +2,28 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
         HashSet<String> set = new HashSet<>() ; // 중복 검사용 Set
-    
+
+        // 초기 상태 
         set.add(words[0]) ; 
         String prevWord = words[0] ; 
-        for(int i = 1 ; i < words.length ; i++){
+        
+        // 완전 탐색 : 단어를 하나씩 검사하면서 탈락자를 찾음
+        int[] answer = new int[]{0,0} ; 
+        for(int i = 1 ; i < words.length ; i++){    
             
-            // 단어를 하나씩 검사하면서 탈락자를 찾음
             String word = words[i] ; 
             
             // 탈락 경우 1: 말했던 단어를 말함 
-           
-            // 탈락 경우 2 : 단어를 잘못 말함  
-            if(!set.add(word) 
-               || !prevWord.endsWith(word.substring(0, 1))) 
-                return new int[]{(i%n)+1, i/n+1}; 
+            if(!set.add(word)   
+               // 탈락 경우 2 : 단어를 잘못 말함  
+               || !prevWord.endsWith(word.substring(0, 1))){
+                answer[0] = i%n + 1;
+                answer[1] = i/n + 1 ; 
+                break; 
+                // {탈락한 사람 번호(나머지), 자신의 몇번째에서 탈락(몫)} + 배열 index가 0부터 시작하니 +1
+            }
             prevWord = word ; 
         } // for 
-        return new int[]{0, 0} ; // 탈락자를 못찾으면 {0,0} 반환 
+        return answer ; // 탈락자를 못찾으면 {0,0} 반환 
     } // solution 
 } // class 
