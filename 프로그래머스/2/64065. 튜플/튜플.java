d199ae587873cb64eee8b798003482397116a2b5
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2024-04-01 16:55:30 +0900

    [level 2] Title: 튜플, Time: 0.83 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,10 +2,8 @@ import java.util.*;
 class Solution {
     public int[] solution(String s) { // 5 이상 1,000,000
         HashMap<Integer, Integer> map = new HashMap<>() ; 
-        String[] tuples = s.split("\\},\\{") ; 
-        
+        String[] tuples = s.replaceAll("[{]"," ").replaceAll("[}]", " ").trim().split(" , ") ; 
         for(String tuple : tuples){ // O(1,000,000) 
-            tuple = tuple.replace("{","").replace("}","") ; 
             String[] nums = tuple.split(",") ; 
             
             for(int i = 0 ; i < nums.length ; i++){
@@ -14,6 +12,7 @@ class Solution {
             }          
         }
         
+        // value 값으로 정렬 (등장횟수가 많은 순으로 튜플 구성)
         List<Integer> keySet = new ArrayList<>(map.keySet()) ; 
         keySet.sort(new Comparator<Integer>(){
             @Override
@@ -21,6 +20,8 @@ class Solution {
                 return map.get(o2).compareTo(map.get(o1)); 
             }
         });
+        
+        // 답 변환 : Map -> Array 
         int[] answer = new int[keySet.size()] ; 
         for(int i = 0 ; i < keySet.size() ; i++){
             answer[i] = keySet.get(i) ; 
