7487f796737cfc1a90bfd074955c5c14f9a7483b
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-02-04 13:20:27 +0900

    [level 2] Title: 괄호 변환, Time: 17.67 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/README.md
+++ 프로그래머스/2/60058. 괄호 변환/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 변환 - 60058 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60058?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60058?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.64 ms
+메모리: 92.4 MB, 시간: 17.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 26일 20:56:54
+2025년 02월 04일 13:20:24
 
 ### 문제 설명
 
--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -1,71 +1,90 @@
-import java.util.* ; 
+import java.util.*;
+
 class Solution {
     public String solution(String p) {
-        // 재귀적으로 수행 (dfs 예상) -  2 이상 1,000 이하 : 작은 데이터값 
-        // 1. u, v로 분리 - u기준 : 괄호 갯수 조건 일치하는 부분까지 u로 포함 
-        // 2. 올바른 괄호인지 확인 + 올바른 괄호로 변환하고 result 문자열에 누적 
-        // 3. v에 대해 1단계부터 다시 재귀적으로 수행 (v가 검사할 문자열인 p로써 기능)
-        
-        return dfs(p) ; 
-    }
-    public String dfs(String p){
-        String result = "" ; 
-        
-        // 종료조건
-        if(p.length() == 0)
-            return p ; 
-        
-        // u, v 분리  
-        String u = "" , v = ""; 
-        int left = 0, right = 0 ; 
-        
-        for(int i = 0 ; i < p.length() ; i++){
-            char c = p.charAt(i) ; 
-        
-            if(c == '('){
-                left++ ;  
-                u += c ; 
-            }else{
-                right++ ; 
-                u += c ; 
-            }
-            if(left == right && i != p.length()-1) {
-                v = p.substring(i+1) ; 
-                break; 
-            }
+        // 문제 : 괄호 개수는 맞지만 짝이 안맞음
+        // 메서드 1 : '올바른 괄호'인지 체크
+        // 메서드 2 : '올바른 괄호'로 변환
+        if(check(p)){
+            return p; 
+        }else{
+            return correct(p);
         }
-        System.out.println("u:"+u);
-        System.out.println("v:"+ v) ; 
-        
-        
-        // u가 올바른 괄호인지 확인  
-        if(rightExp(u)) 
-            result +=  u + dfs(v) ; 
-        else{
-        // 올바른 괄호가 아니라면 올바른 괄호로 변환
-            result += "(" + dfs(v) +")"; 
-            for(int i = 1 ; i < u.length()-1 ; i++){
-                char c = u.charAt(i) ; 
-                if(c == '(')
-                    result += ")" ; 
-                else 
-                    result += "(" ; 
-            }
-        }        
-        return result ; 
     }
     
-    public boolean rightExp(String u){
-        Stack<Character> stack = new Stack<>() ; 
-        for(int i = 0 ; i < u.length() ; i++){
-            char c = u.charAt(i) ; 
-            if(c == ')' ){
-                if(stack.isEmpty()) return false ; 
-                else  stack.pop() ; 
-            }  
-            else 
-                stack.push(c) ; 
+    public boolean check(String p){ // 재귀함수
+        
+        if(p.length()==0)
+            return true; 
+        
+        Stack<String> s = new Stack() ;
+        for(int i = 0 ; i < p.length() ; i++){
+            
+            if(p.charAt(i)=='('){
+                s.push(p.charAt(i)+"");
+            }else{
+                try{
+                    s.peek();
+                }catch(Exception e){
+                    return false;
+                }
+                s.pop();
+            }            
         }
-        return stack.size() == 0 ? true : false ;
-    }
+        if(s.size()==0)
+            return true ;
+        return false ;
+    } // check()
+    
+    public String correct(String p){
+        
+        if(p.length()==0)
+            return p ;
+        
+        int index = find(p) ;
+        String u = p.substring(0,index+1);
+        String v = "" ;
+        if(index < p.length()-1)
+            v = p.substring(index+1);
+        
+        if(check(u)){
+            v = correct(v) ;
+            return u + v ;
+        }else{
+            v = "(" + correct(v) + ")" ; 
+            u = reverse(u);
+            return v + u;
+        }
+    } // correct()
+    
+    public int find(String p){
+        int t = 0;
+        for(int i = 0 ; i < p.length() ; i++){
+            
+            if(p.charAt(i) == '('){
+                t+=1;
+            }else{
+                t-=1;
+            } 
+            if(t==0)
+                return i ;
+        }
+        return p.length();
+    } // find()
+    
+    public String reverse(String u){
+        if(u.length() < 3)
+            return ""; 
+        
+        u = u.substring(1,u.length()-1) ;
+            
+        String ret = "";
+        for(int i = 0 ; i < u.length() ; i++){
+            if(u.charAt(i)=='(')
+                ret+=")";
+            else
+                ret+="(";
+        }
+        return ret;
+    } // reverse()
 }
