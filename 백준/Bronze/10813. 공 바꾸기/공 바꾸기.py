02414a3e3f7b93584c46ca66a955d8dec1a73001
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-07-15 14:39:52 +0900

    [Bronze II] Title: 공 바꾸기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -3,9 +3,15 @@ bags = [i for i in range(N+1)]
 
 for _ in range(M):
     i, j = map(int, input().split())
-    tmp = bags[i] 
-    bags[i] = bags[j] 
-    bags[j] = tmp 
+
+    # 1. swap 직접 
+#    tmp = bags[i] 
+#    bags[i] = bags[j] 
+#    bags[j] = tmp 
+    
+    # 2. tuple unpacking
+    bags[i], bags[j] = bags[j], bags[i]
+#   (bags[i], bags[j]) = (val1, val2) --> 튜플 객체 생성 후 대입(언패킹) 
     
 print(*bags[1:])
 
