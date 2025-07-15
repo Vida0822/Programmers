67218cf12fc81bdc00f4e00b6fbdaaf4c87ae121
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-07-15 13:53:04 +0900

    [Bronze III] Title: 최소, 최대, Time: 316 ms, Memory: 128172 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,5 +1,8 @@
 N = int(input())
-nums = list(map(int, input().split())) # map 객체는 indexing이 안되는거 명심 !! (단순히 iterator로 순차반환만)
+nums = list(map(int, input().split()))
+# ※1. map 객체는 indexing이 안되는거 명심 !! (단순히 iterator로 순차반환만)
+# ※2. Iterator는 실행(순차 반환)할 때 소비(요소 삭제)되어 재사용 불가능 --> list로 변환해 여러번 참조하도록
+
 # O(N)
 # 1. 직접 구현 
 # max_val, min_val = -1000001 , 1000001
@@ -12,4 +15,5 @@ nums = list(map(int, input().split())) # map 객체는 indexing이 안되는거 
 #print(min_val, max_val)
 
 # 2. max(), min() 사용 
+# max(iterable) - iterable : 리스트, 문자열, 튜플, map 객체
 print(min(nums), max(nums))
