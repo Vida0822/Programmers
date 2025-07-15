c4f0083565f9103c7e7765afa89276a05f7e9d07
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-07-15 15:38:42 +0900

    [Bronze I] Title: 단어 공부, Time: 320 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,9 +1,9 @@
-word = input()
+word = input().upper()
 chars = dict()
 
 max = 0 
 for w in word : 
-    w = w.upper()
+    w = w
     chars[w] = chars.get(w,0) + 1 
     
     if chars[w] > max : 
@@ -13,11 +13,21 @@ ans = list()
 for key, val in chars.items() : 
     if val == max : 
         ans.append(key)
+# for c in chars.items() :  --> indexing 도 가능! 
+#    if c[1] == max : 
+#         ans.append(c[0])
+
+# 축약 
+# ans = [k for k, v in chars.items() if v == max]
+
 
 if len(ans) == 1 : 
     print(*ans)
 else : 
     print('?')
-        
+    
+# 축약 
+# print(ans[0] if len(ans) == 1 else '?')
+    
         
     
