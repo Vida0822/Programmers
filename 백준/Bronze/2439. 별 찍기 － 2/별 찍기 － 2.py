3052e9ac9fb222ce9d4a08ec6cac5b7895c82d7a
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-07-15 12:35:50 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
@@ -1,8 +1,10 @@
 N = int(input()) 
 
 for i in range(1, N+1) :
-    for _ in range(N-i) : 
-        print(' ', end='')
-    for _ in range(i) : 
-        print('*', end='')
-    print() 
+    #for _ in range(N-i) : 
+    #    print(' ', end='')
+    #for _ in range(i) : 
+    #    print('*', end='')
+    #print()
+    
+    print(' '*(N-i), '*'*i, sep='') 
