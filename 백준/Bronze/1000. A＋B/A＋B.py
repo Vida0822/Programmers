a81c0166cc737911c73f1802aa182c2e97f7f3ba
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-07-15 10:52:31 +0900

    [Bronze V] Title: A+B, Time: 48 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.py
@@ -0,0 +1,15 @@
+# A = input() 
+# B = input()
+
+# 1. 
+A, B = map(int, input().split()) 
+print(A+B) 
+# 1) input().split() : 입력받은 값을 공백 기준 분리 (String[]) 
+# 2) 각각의 요소값을 int로 mapping 
+# 3) 해당 요소를 순서대로 A, B 에 대입 
+
+# 2. 
+# AB = input().split() 
+# print(int(AB[0])+int(AB[1]))
+# 1) input().split() : 입력받은 값을 공백 기준 분리 (String[]) 
+# 2) 해당 요소를 순서대로 A, B 에 대입     
