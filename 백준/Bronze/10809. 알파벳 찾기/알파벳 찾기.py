fb63d7c77f990c373d15bd83cc619a63ffe3dde9
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-07-16 16:16:11 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -4,7 +4,7 @@ map = dict()
 for i in range(len(S)): 
     map[S[i]] = map.get(S[i], i) 
 
-# 숫자 -> 문자 : chr() 
+# 코드 -> 문자 : chr() 
 for j in range(97, 123) : # 소문자 ASKII CODE
     print(map.get(chr(j), -1), end=' ')
 
