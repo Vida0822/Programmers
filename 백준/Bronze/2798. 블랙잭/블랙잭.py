4fd990bf6f076dcf4a27ffd66b7267ab74edfc15
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-07-18 11:46:49 +0900

    [Bronze II] Title: 블랙잭, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,19 +1,23 @@
 N, M = map(int, input().split()) 
 cards = list(map(int, input().split()))
 
-# 1. 카드 정렬 
-cards.sort() 
+# [조합]
+# cards.sort() --> 완전탐색이므로 정렬 필요 X 
 
-# 2. 완전 탐색 (3중 반복문)
+# 1. 완전 탐색, 3중 반복문
 # 카드의 합이 M이하인 모든 경우를 구하고, 그중 가장 큰 값을 반환 
 result = 0 
 
 # 코드 기억 : 중복 없는 모든 조합을 구하는 코드 
-for i in range(N) : 
-    for j in range(i+1, N) : 
-        for k in range(j+1, N) : 
-            if cards[i]+cards[j]+cards[k] <= M :
-                result = max(result, cards[i]+cards[j]+cards[k])
+
+
+# 2. permutaions, combinations(Intertools) : 순열, 조합 라이브러리                
+
+from itertools import combinations 
+
+for card in combinations(cards, 3) : # cards에서 3개를 뽑는 조합 리스트 
+    if result < sum(card) <= M : 
+        result = sum(card)
 
 print(result)    
     
