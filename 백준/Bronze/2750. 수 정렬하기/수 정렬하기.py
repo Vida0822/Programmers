ec8458b6fee4f556d26578886509bb332143c37b
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-07-18 10:57:14 +0900

    [Bronze II] Title: 수 정렬하기, Time: 124 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -4,11 +4,19 @@ nums = []
 for i in range(N) : 
     nums.append(int(input()))
 
-# Bubble sort : 이웃끼리 비교 
-for i in range(N-1, 0 , -1) : 
-    for j in range(i) : 
-        if nums[j] > nums[j+1] : 
-            nums[j], nums[j+1] = nums[j+1], nums[j]
-            
+# 1. Bubble sort : 이웃끼리 비교 --> O(N^2)
+
+                
+# 2. Selection Sort : 최소값을 찾고 맨앞으로 보내기 
+for i in range(N-1) :
+    mindex = i  
+    for j in range(i, N) : 
+        if nums[j] < nums[mindex] : 
+            mindex = j 
+    nums[i],nums[mindex] = nums[mindex],nums[i]     
+
+
+# 3. Inserstion Sort
+
 for n in nums : 
     print(n)
