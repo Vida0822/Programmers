a9942b49fac746989b66e214d3bd8ed9bb702fcc
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-07-20 16:36:41 +0900

    [Silver V] Title: 벚꽃이 정보섬에 피어난 이유, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/17127. 벚꽃이 정보섬에 피어난 이유/벚꽃이 정보섬에 피어난 이유.py
+++ 백준/Silver/17127. 벚꽃이 정보섬에 피어난 이유/벚꽃이 정보섬에 피어난 이유.py
@@ -6,6 +6,9 @@ A = list(map(int, input().split()))
 # 완전 탐색 ? 4개로 나누는 조합을 모두 구하고, 가장 큰 값을 반환 
 # N이 10 이하이기 때문에 가능 
 ans = 0 
+# 각각의 반복 변수가 각 나무에 포함되는 번호의 '끝' 인덱스
+
+# O(N^3) * O(N) : 리스트에 있는 숫자 더하기
 for i in range(1, N-2) : 
     for j in range(i+1, N-1) : 
         for k in range(j+1, N) : # N-2  
