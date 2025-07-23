17c0339d99acff882169c68dd773f92e8cd81a64
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-07-23 09:56:46 +0900

    [Bronze III] Title: ACM 호텔, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -2,7 +2,15 @@ T = int(input())
 
 for _ in range(T):
     H, W, N = map(int, input().split())
-    h = N % H if N%H!=0 else N%H+H
-    w = N // H + 1 if N%H!=0 else N//H
+    
+    # h, w : 변수 의미 파악이 어려움, 도메인에 맞는 변수명 사용 better 
+    floor = N%H 
+    room = N//H +1 
+    
+    if floor==0 : 
+        floor = H 
+        room -= 1 
 
-    print(h, '%02d' % w, sep='')
+#    print(h, '%02d' % w, sep='') --> 이 형태보다는 f-string 사용하는게 현대적
+    print(f'{floor}{room:02}') # f{변수:형식지정자}  
+    
