b1b43faae578744ec091c2c7f4ab7f4fd2af3837
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-07-25 14:29:29 +0900

    [Silver V] Title: 기상캐스터, Time: 100 ms, Memory: 110856 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.py
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.py
@@ -5,23 +5,39 @@ arr = [input() for _ in range(H)]
 times = [[-1]*W for _ in range(H)]
 
 # 1. Flag를 이용한 분기
+# for h in range(H) :
+#    flag = False # 해당 행에 구름을 만났는지 여부
+#    sm = 0
+#    for w in range(W) :
+#        if arr[h][w] == 'c':
+#            times[h][w] = 0
+#            if not flag :
+#                flag = True
+#            if flag :
+#                sm = 0
+#        else:
+#            if not flag :
+#                times[h][w] = -1
+#            if flag :
+#                sm += 1
+#                times[h][w] = sm
+
+
+# 2. 불필요한 중복 코드 정리 
 for h in range(H) :
     flag = False # 해당 행에 구름을 만났는지 여부
-    sm = 0
+
     for w in range(W) :
-        if arr[h][w] == 'c':
+        if arr[h][w] == 'c': # 구름을 만났는데
             times[h][w] = 0
-            if not flag :
-                flag = True
-            if flag :
-                sm = 0
-        else:
-            if not flag :
-                times[h][w] = -1
-            if flag :
-                sm += 1
+            sm = 0
+            flag = True
+
+        else: # 구름이 없는 곳인데
+            if flag: # 구름이 있었다면
+                sm += 1 # 한칸씩 +1 값 대입
                 times[h][w] = sm
 
 for time in times :
     print(*time)
-
+    
