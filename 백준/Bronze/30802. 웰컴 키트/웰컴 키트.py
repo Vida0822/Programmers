eeca433f3e9e08b0d1713c998244b3f0d25d85dd
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-07-26 13:46:46 +0900

    [Bronze III] Title: 웰컴 키트, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
@@ -8,5 +8,8 @@ T, P = map(int, input().split())
 #for l in lst : 
 #    t_muk += math.ceil(l/T)
 #print(t_muk)
+
+# math.ceil(): 가장 가까운 정수로 up (정수 그 자체인 경우 올림X)
+#     ex. math.ceil(0) --> 0 
 print(sum(math.ceil(l/T) for l in lst))
 print(N//P, N%P)
