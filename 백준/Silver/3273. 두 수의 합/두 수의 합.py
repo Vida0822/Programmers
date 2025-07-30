1149e7cae9eaa91623ea6ad2f0d83f551e89c3d6
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-07-30 16:53:57 +0900

    [Silver III] Title: 두 수의 합, Time: 120 ms, Memory: 129676 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,3 +1,16 @@
+"""
+풀이 시간 : 10분 (구상 : 5분, 디버그 : 5분)
+
+[구상]
+N = 100000로 크기 때문에 O(N) 또는 O(N log N)의 로직 필요
+=> set 사용 : A 가 서로 다른 자연수이기 때문에
+
+1) A의 요소 중 a를 순차적으로 탐색하면서
+2) 찾고자 하는 수 X에서 (X-a)를 구하면 합하면 X가 되는 자연수 쌍
+3) X-a가 A에 있으면 자연수 쌍이 있는 것 --> cnt += 1
+4) cnt // 2 : 중복 검사 (2번) 
+
+"""
 N = int(input())
 A = set(map(int, input().split()))
 X = int(input())
@@ -6,7 +19,7 @@ ans = 0
 for a in A:
     b = X-a
 
-    if a != b and  b in A:
+    if a != b and b in A:
         ans += 1
 
 print(ans//2)
