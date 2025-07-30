9132af26dfc3a5ce96eaef6633c7d282f98a2af7
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-07-30 19:48:15 +0900

    [Silver V] Title: 색종이, Time: 100 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,18 +1,30 @@
+"""
+풀이 시간 : 15분 (구상&구현 : 5분, 디버깅 : 10분)
+
+[구상]
+완전 탐색 가능 (색종이 칠하기 : O(10x10x100) + 좌표 중 '1' count : O(100x100)
+1) 색종이 칠하기  : si(입력2) ~ si+10 ,  sj(입력1)~sj+10
+2) 개수 cnt : arr[i][j] == 1
+
+=> refactoring : 개수를 색종이 칠하면서 count 가능
+
+[디버깅]
+좌표에 대입할 때 '=' 써야하는데 계속 '=='를 씀 ㅠ 
+"""
 N = int(input())
-metrix = [[0]*101 for _ in range(101)]
 
-for i in range(N) : 
-    X, Y = map(int, input().split())  
-    
-    for x in range(X, X+10) : 
-        for y in range(Y, Y+10) : 
-            metrix[x][y] = 1 
-            
-width = 0 
-for x in range(101) :  # O(100^2)
-    for y in range(101) : 
-        if metrix[x][y] == 1  :
-            width += 1 
-print(width)        
+arr = [[0]*100 for _ in range(100)]
+cnt = 0
+for _ in range(N) :
+    sj,  si = map(int, input().split())
 
-            
+    for i in range(si, si+10) : # si ~ si+9
+        for j in range(sj, sj+10):
+            if arr[i][j] == 0: # 만약 칠해져있지 않다면
+#                arr[i][j] == 1 # 이거 계속 실수 ㅠㅠ
+                arr[i][j] = 1 # 새롭게 칠하고
+                cnt += 1 # 검은 영역 개수 count
+print(cnt)
+
+
+
