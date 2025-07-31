cc7a230bbdc5bbb01f2210e526f1cc0b2cbe8838
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-07-31 14:27:27 +0900

    [Silver I] Title: 숨바꼭질, Time: 136 ms, Memory: 126948 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,7 +1,16 @@
 """
 1차 시도 : 메모리 초과...
+2차 시도 : 성공 (154144KB, 184ms )
 
+[구상]
+특정 위치(x)에서 갈 수 있는 좌표는 x-1, x+1, x+X --> 각각을 요소로 그래프 연결
+=> 그렇게 그래프 연결해가다가 K가 되면 return
+
+[디버깅]
+- x가 될 수 있는 0 <= nx <= 100000 외의 좌표까지 방문해서 메모리 초과 발생
+=> 방문 배열, 큐에 넣을 수 제한
 """
+
 from collections import deque
 import sys
 input = sys.stdin.readline
@@ -28,7 +37,7 @@ while q:
     for dx in (-1, 1, x) :
         nx = x + dx
         if nx not in v :
-            if nx <= 100000 :
+            if 0 <= nx <= 100000 : #  debug: 불필요하게 가능한 범위 외 수까지 방문 배열에 넣어서 생기는 문제
                 v.add(nx)
                 q.append((nx, t+1))
 else:
