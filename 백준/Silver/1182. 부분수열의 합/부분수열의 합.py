46cf395f468e879b7c9c8b9b4f967c9cca552496
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-08-05 17:12:33 +0900

    [Silver II] Title: 부분수열의 합, Time: 256 ms, Memory: 111804 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,27 +1,26 @@
-def dfs(n, i, ans):
-    global cnt
+"""
+강사님 코드 1 
+: 부분 수열/집합 정석 코드 --> 포함/미포함
+"""
+
+def dfs(n, sm, cnt):
+    global ans
 
     # [0] 종료 조건
-    if n == k:
-        if sum(ans) == S: # 정답 조건
-            cnt += 1
+    if n == N:
+        if cnt > 0 and sm == S: # cnt > 0 : 크기가 양수인 부분 수열
+            ans += 1
+        return
 
     # [1] 재귀 호출
-    for j in range(i+1, N+1):
-        if v[j] == 0:
-            v[j] = 1
-            ans.append(lst[j])
-            dfs(n+1, j, ans)
-            v[j] = 0
-            ans.pop()
+    dfs(n+1, sm+lst[n], cnt+1) # 선택
+    dfs(n+1, sm, cnt) # 선택 X
 
 
 N, S = map(int, input().split())
-lst = [0]+list(map(int, input().split()))
-cnt = 0
+lst = list(map(int, input().split()))
 
-for k in range(1, N+1):
-    v = [0]*(N+1)
-    dfs(0, 0, [])
+ans = 0
+dfs(0, 0, 0)
 
-print(cnt)
+print(ans)
