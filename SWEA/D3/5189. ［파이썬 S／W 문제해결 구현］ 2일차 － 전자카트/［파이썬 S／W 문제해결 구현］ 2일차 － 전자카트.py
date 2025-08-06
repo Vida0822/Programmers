b8f16b2671047583e57db71b8ac3f4bbc00990b2
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-08-06 12:15:21 +0900

    [D3] Title: [파이썬 S/W 문제해결 구현] 2일차 - 전자카트, Time: 267 ms, Memory: 62,080 KB -BaekjoonHub

--- SWEA/D3/5189. ［파이썬 S／W 문제해결 구현］ 2일차 － 전자카트/［파이썬 S／W 문제해결 구현］ 2일차 － 전자카트.py
+++ SWEA/D3/5189. ［파이썬 S／W 문제해결 구현］ 2일차 － 전자카트/［파이썬 S／W 문제해결 구현］ 2일차 － 전자카트.py
@@ -1,29 +1,38 @@
-def dfs(n, i, sm, v): # 검사 위치, 이전 구역, 점수 합계, 포함 개수
-    global ANS
+"""
+강사님 코드
+: 패딩 사용
 
-    # 가지 치기
-    if sm >= ANS:
-        return
+N: 방문한 구역 수
+--> 종료 조건 : n == N
+"""
+
+def dfs(n, sm, cur):
+    global ans
+
+    # if ans <= sm+arr[cur][1]:
+    #     return
+    # -> FAIL: 다 돌지 않았는데 1로 복귀하는 비용이 엄청 커서 ans 값을 초과한 경우 검사를 종료해버림
+    #          (실제로 몇군데 들려 돌아가다보면 ans 값보다 작을 수 있는데! )
 
     # [0] 종료 조건
-    if n == N-1: # debug : N->N-1 ) 사무실은 구역에서 제외되므로 최종 목표 구역 개수에서 빼줘야한다. 
-        ANS = min(ANS, sm+bat[i][0])
+    if n == N :  # 모든 구역 방문
+        ans = min(ans, sm+arr[cur][1])
         return
 
-    # [1] 재귀 호출
-    for j in range(1, N):
+    for j in range(2, N+1):
         if v[j] == 0:
-            # print(bat[i][j])
             v[j] = 1
-            dfs(n+1, j, sm+bat[i][j], v)
+            dfs(n+1, sm+arr[cur][j] ,j)
             v[j] = 0
 
-T = int(input())
-for t in range(1, T+1):
+TC = int(input())
+for tc in range(1, TC+1):
     N = int(input())
-    bat = [list(map(int, input().split())) for _ in range(N)]
 
-    ANS = 100*N # N? N**2?
-    dfs(0, 0, 0, [0]*(N+1))
+    arr = [[0]*(N+1)]+[[0]+list(map(int, input().split())) for _ in range(N)]
+    ans = 100*N
 
-    print(f'#{t} {ANS}')
+    # 구역수, 비용합, 기준구역번호
+    v = [0]*(N+1)
+    dfs(1, 0, 1)
+    print(f'#{tc} {ans}')
