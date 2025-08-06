865d51a49846154c1adf0787deab31a440906536
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-08-06 14:45:34 +0900

    [Silver IV] Title: 대피소, Time: 128 ms, Memory: 109544 KB, Score: 17 point -BaekjoonHub

--- 백준/Silver/28215. 대피소/대피소.py
+++ 백준/Silver/28215. 대피소/대피소.py
@@ -5,13 +5,13 @@ def dfs(n, i, ans):
     if n == K:
         # 정답 처리
         dis = [200000]*N
-        for si, sj in ans:
-            for h in range(len(H)):
+        for si, sj in ans: # 각 대피소에서
+            for h in range(len(H)): # 모든 집으로의 거리를 구하고
                 ei, ej = H[h]
-                dis[h] = min(dis[h], abs(si-ei)+abs(sj-ej))
+                dis[h] = min(dis[h], abs(si-ei)+abs(sj-ej)) # 그 중 가장 작은 값을 해당 집과 대피소와의 거리로 설정
                 # print(si, sj, ei, ej, dis[h])
 
-        t = max(dis)
+        t = max(dis) # 그 거리 중 가장 큰 값 갱신 
         if t < ANS_MIN:
             ANS_MIN = t
         return
@@ -26,6 +26,6 @@ def dfs(n, i, ans):
 N, K = map(int, input().split())
 H = [tuple(map(int, input().split())) for _ in range(N)]
 
-ANS_MIN = 200000
+ANS_MIN = 200001
 dfs(0, 0, [])
-print(ANS_MIN if ANS_MIN != 200000 else 0) # edge case : N == K
+print(ANS_MIN if ANS_MIN != 200001 else 0) # edge case : N == K
