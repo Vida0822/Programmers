549aea9965c1c4abbd54d28595c3f24ed3cb2ea1
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-08-06 09:49:56 +0900

    [D3] Title: [파이썬 S/W 문제해결 구현] 5일차 - 최소 생산 비용, Time: 692 ms, Memory: 62,208 KB -BaekjoonHub

---
+++ SWEA/D3/5209. ［파이썬 S／W 문제해결 구현］ 5일차 － 최소 생산 비용/［파이썬 S／W 문제해결 구현］ 5일차 － 최소 생산 비용.py
@@ -0,0 +1,27 @@
+def dfs(n, sm, v):
+    global ANS
+
+    # [0] 종료 조건
+    if n == N:
+        ANS = min(ANS, sm)
+        return
+
+    # 가지 치기 (debug)
+    if sm > ANS :
+        return
+
+    # [1] 재귀 호출
+    for j in range(N):
+        if v[j] == 0:
+            v[j] = 1
+            dfs(n+1, sm+arr[n][j], v)
+            v[j] = 0
+
+T = int(input())
+for t in range(1, T+1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    ANS = 99*N
+    dfs(0, 0, [0]*N)
+    print(f'#{t} {ANS}')
