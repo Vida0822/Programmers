5e7244e6f50106db9fbe17ad23dbd52b204f2aa7
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-08-07 10:11:36 +0900

    [Bronze II] Title: 수 정렬하기, Time: 104 ms, Memory: 110760 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,30 +1,35 @@
-def dfs(lst):
-
-    # [0] 종료 조건
-    if not lst : # debug : pop from empty list --> left, right가 빈 리스트일 때 아래애서 그냥 pop을 시도
-        return []
-    if len(lst) == 1:
-        return [lst.pop()]
-
-    # [1] 단위 작업
-    piv = lst.pop()
-    left = []
-    right = []
-
-    for l in lst:
-        if l <= piv :
-            left.append(l)
-        else:
-            right.append(l)
-
-    # [2] 재귀 호출 
-    return dfs(left) + [piv] + dfs(right)
-
+"""
+퀵 정렬 직접 구현 : 128 ms
+sort() 사용 :
+"""
+# def dfs(lst):
+#
+#     # [0] 종료 조건
+#     if not lst : # debug : pop from empty list --> left, right가 빈 리스트일 때 아래애서 그냥 pop을 시도
+#         return []
+#     if len(lst) == 1:
+#         return [lst.pop()]
+#
+#     # [1] 단위 작업
+#     piv = lst.pop()
+#     left = []
+#     right = []
+#
+#     for l in lst:
+#         if l <= piv :
+#             left.append(l)
+#         else:
+#             right.append(l)
+#
+#     # [2] 재귀 호출
+#     return dfs(left) + [piv] + dfs(right) # 단일 값도 리스트로 묶어줘야함 주의
+#
 
 N = int(input())
 A = [int(input()) for _ in range(N)]
-ans = dfs(A)
+# ans = dfs(A)
+A.sort()
 
-for a in ans:
+for a in A:
     print(a)
 
