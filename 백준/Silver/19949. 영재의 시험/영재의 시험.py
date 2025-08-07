1ba338abb1f8eb719ce71a06af6af378a7547b3a
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-08-07 19:56:44 +0900

    [Silver II] Title: 영재의 시험, Time: 244 ms, Memory: 113188 KB -BaekjoonHub

--- 백준/Silver/19949. 영재의 시험/영재의 시험.py
+++ 백준/Silver/19949. 영재의 시험/영재의 시험.py
@@ -1,41 +1,30 @@
-def dfs(n, i, scr):
+"""
+Refactoring : 전선택과 전전선택을 재귀함수의 인자로 넘겨줌 (계속 업데이트)
+"""
+
+def dfs(n, scr, bbv, bv):
     global ANS
+
+    # 가지치기 : 남은 문제(총문제-푼문제)+현재점수 < 5 --> 고려할 필요 X
+    if 10 - n + scr < 5:
+        return
 
     # [0] 종료조건
     if n == N:
         # print(v)
-        if scr >= 5 :
-            ANS += 1 # 정답 처리
+        # if scr >= 5 : --> 위에서 가지치기해줬기때문에 5점 넘는지 확인할 필요 X
+        ANS += 1 # 정답 처리
         return
 
     # [1] 재귀호출
     for j in range(1, 6):
-        if v[j] >= 2 : # 해당 답이 두번 연속했으면
-            continue # 그 답은 pass~!
-        else:
-            if i == j  :
-                v[j] += 1
-                if A[n] == j :
-                    dfs(n+1, j, scr+1)
-                else :
-                    dfs(n+1, j, scr)
-                v[j] -= 1
-
-            else:
-                t = v[i]  # 직전 답의 연속 개수를 저장해두고
-                v[i] = 0
-                v[j] += 1 # 현재 답의 연속 개수를 1증가 (i == j 일 수도 있다)
-                if A[n] == j :
-                    dfs(n+1, j, scr+1)
-                else :
-                    dfs(n+1, j, scr)
-                v[j] -= 1
-                v[i] = t # 원상 복구
+        if j == bv and j == bbv : continue
+        dfs(n+1, scr+int(A[n] == j), bv, j)
 
 A = list(map(int, input().split()))
 N = len(A)
 ANS = 0
 
-v = [0]*6
-dfs(0, 0, 0)
+# v = [0]*6
+dfs(0, 0, 0, 0)
 print(ANS)
