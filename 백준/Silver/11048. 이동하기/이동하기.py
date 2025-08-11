a71ea4297b7ae6f11bda6d04de848331fde1a698
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-08-11 17:28:08 +0900

    [Silver II] Title: 이동하기, Time: 196 ms, Memory: 111760 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -1,20 +1,18 @@
+"""
+강사님 코드 
+: 1차원 dp테이블 --> 불필요한 메모리 낭비 방지 
+"""
 N, M = map(int, input().split())
+# A = [[0]*(M+1)] + [[0]+list(map(int, input().split())) for _ in range(N)]
 
-# A = [0]*(N+1) + [[0]+list(map(int, input().split())) for _ in range(N)] # 패딩 필요
-A = [[0]*(N+1)] + [[0]+list(map(int, input().split())) for _ in range(N)] # 패딩 debug... 행 패딩 붙일때 2차원 배열로 만들어서 붙여야함 주의
-
-
-# [0] 테이블 정의
-dp = [[0]*(M+1) for _ in range(N+1)]
-
-# [1] 초기값
-dp[1][1] = A[1][1]
-
-# [2] 점화식 -->  O(N*M)
-for i in range(1, N+1):
+dp = [0]*(M+1)
+for _ in range(1, N+1):
+    lst = [0]+list(map(int, input().split()))
     for j in range(1, M+1):
-        dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) # 패딩 있기때문에 범위체크 X
-        dp[i][j] += A[i][j]
+        dp[j] = lst[j] + max(dp[j], dp[j-1])
+        # 1) dp[j] : i-1행에서 j열까지의 최대값 == dp[i-1][j]
+        # 2) dp[j-1] : i행에서 j-1열까지의 최대값  == dp[i][j-1]
 
-# [3] 답 부분 출력
-print(dp[N][M])
+print(dp[M])
+
+
