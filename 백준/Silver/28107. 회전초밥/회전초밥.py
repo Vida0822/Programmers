aa71baa4a91956502679f2e727bb3527cdcfd11c
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-08-12 15:17:00 +0900

    [Silver I] Title: 회전초밥, Time: 360 ms, Memory: 158944 KB -BaekjoonHub

--- 백준/Silver/28107. 회전초밥/회전초밥.py
+++ 백준/Silver/28107. 회전초밥/회전초밥.py
@@ -1,27 +1,29 @@
+"""
+강사님 코드
+: dictionary 를 사용하지 않고 메뉴번호 최대개수만큼 리스트 생성 후 look up
+"""
+
 from heapq import heappush, heappop
 
 N, M = map(int, input().split())
-A = {}
+# [1] 우선순위큐(메뉴번호별) 생성 후 고객 번호 push
 
-# 1. 각 초밥 종류 별 원하는 손님 저장 (heapq)
+hq = [[] for _ in range(200_001)] # 메뉴 최대개수
+
+# 1. 각 초밥 종류 별 원하는 손님 저장 (heapq) : 가장 작은 손님번호가 우선하게 됨
 for i in range(N) :
     types = list(map(int, input().split()))
-    k = types.pop(0)
-
-    for t in types:
-        hq = A.get(t, [])
-        heappush(hq, i)
-        A[t] = hq
+    for n in types[1:]:
+        heappush(hq[n], i)  # 메뉴번호 n의 큐에 고객번호 i추가
 
 # print(A)
 
 # 2. 만들어진 초밥 종류별 1순위 손님 반환
+lst = list(map(int, input().split()))
 ANS = [0]*N
-B = list(map(int, input().split()))
-for t in B:
-    if t in A and A[t] :
-        i = heappop(A[t])
-        ANS[i] += 1
+for menu in lst:
+    if hq[menu]: # debug: index error (입력할때는 요소값이 있었더라도 pop한 후 빈 리스트가 되었을 수 있다)
+        ANS[heappop(hq[menu])] += 1
 
 print(*ANS)
 
