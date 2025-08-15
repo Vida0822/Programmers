a02a612f98b6423349958c00725b5e0d51512eab
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-08-15 22:54:41 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,9 +1,13 @@
 N, K = map(int, input().split())
-q = [i for i in range(1, N+1)]
+q = [str(i) for i in range(1, N+1)]
 
 i = 0
 L = len(q)
 ans = []
+"""
+O(N) = O(1000): 한번 반복돌때마다 숫자하나씩 빼고, 그 숫자가 없어질때까지 반복 
+=> 완탐은 비효율적 (특정 인덱스에서 i+K전까지 지나가서 i+K번째 pop) 
+"""
 while q :
     i = (i+(K-1))%L
     # 규칙성 발견 어렵, 변화하는 각 단계별 큐 양상 + 인덱스 같이 체크해야함 (다음에도 이런 손코딩하기)
@@ -11,7 +15,12 @@ while q :
     ans.append(q.pop(i))
     # print(q.pop(i))
 
-print('<', end='')
-print(*ans, sep= ', ', end='')
-print('>', end='')
+# Refactoring : join()
+# print('<', end='')
+# print(*ans, sep= ', ', end='')
+# print('>', end='')
+print('<'+', '.join(ans)+'>')
+
+
+
 
