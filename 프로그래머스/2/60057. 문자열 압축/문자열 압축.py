68419fe58c60411b7918c8e78a9f93ae2b5b6d13
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-08-27 23:41:27 +0900

    [level 2] Title: 문자열 압축, Time: 2.92 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,27 +1,50 @@
-def solution(s):
-    answer = len(s)
-    
-    # O(N^2) = O(1000^2)
-    for i in range(1,len(s)//2 + 1) :  # O(N/2) : 단위를 1~N/2
-        prev = s[:i]
-        count = 1 
-        compressed = ""
-        
-        for j in range(i,len(s),i): # O(N/i) : 매번 한글자~N/2글자 하나하나 확인 
-            next = s[j:j+i]
-            
-            if prev == next : 
-                count += 1 
-            else :
-                # 압축
-                compressed += str(count) + prev if count >= 2 else prev
-                
-                # 초기화 
-                count = 1 
-                prev = next 
+def solution(S):
+    # [0] 시뮬레이션 준비
+    N = len(S)
 
-        # 남은 문자 압축 
-        compressed += str(count) + prev if count >= 2 else prev 
-        
-        answer = min(answer, len(compressed))
+    # debug = 0
+    # print(S, N)
+
+    # [1] 시뮬레이션 실행
+    answer = 1000  # 최소 문자열 길이
+
+    # 0) 자를 단위 정하고
+    for k in range(1, N + 1):
+        cnt = 1
+        prev = ''
+        new_S = ''
+        # debug = 1
+        # print(prev)
+        # print(round(N / k))
+        for i in range(N // k+2):
+            # 1) 문자 추출
+            cur = S[i * k:(i + 1) * k]
+            # debug = 2
+            # print(cur)
+
+            # 2) 이전 문자 압축
+            # print('bef',new_S)
+            if prev == cur:
+                cnt += 1
+                # debug = 3
+                # print(f"cnt={cnt}, prev={prev}, cur={cur}")
+
+            else:
+                if cnt > 1:
+                    new_S += (str(cnt) + prev)
+                elif cnt == 1:
+                    new_S += prev
+                cnt = 1
+                prev = cur
+                # debug = 4
+                # print(new_S)
+
+            # debug = 5
+            # print('aft',new_S)
+
+        # 4) min 갱신
+        answer = min(answer, len(new_S))
+        # debug = 6
+        # print(answer, new_S, len(new_S))
+
     return answer
