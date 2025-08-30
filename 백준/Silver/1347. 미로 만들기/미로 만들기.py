f4b18e56c8765fefcf1b646aa1f9e6736789afe2
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2025-08-31 00:27:40 +0900

    [Silver II] Title: 미로 만들기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1347. 미로 만들기/미로 만들기.py
+++ 백준/Silver/1347. 미로 만들기/미로 만들기.py
@@ -10,6 +10,9 @@
         --> 지도 확장
 - R, L
     - cd = (cd+1)%4
+
+Refactoring : 방문 배열을 따로 만들지 않고, 바로 지도(arr)에 '.' 표시
+=> 포함 부분만 출력
 """
 
 
@@ -18,9 +21,9 @@ K = int(input())
 orders = input()
 delta = ((0, -1), (-1, 0), (0, 1), (1, 0))
 
-v = [[0]*100 for _ in range(100)]
+arr = [['#']*100 for _ in range(100)]
 ci, cj, cd = 50, 50, 3
-v[ci][cj] = 1
+arr[ci][cj] = '.'
 
 # [1] 실행
 # 1. 이동하기
@@ -31,11 +34,12 @@ for o in orders:
         cd = (cd-1)%4
     elif o == 'R':
         cd = (cd+1)%4
+        
     else :
         # F : 전진
         di, dj = delta[cd]
         ni, nj = ci+di, cj+dj
-        v[ni][nj] = 1
+        arr[ni][nj] = '.'
 
         up = min(up, ni)
         dwn = max(dwn, ni)
@@ -45,17 +49,8 @@ for o in orders:
         ci, cj = ni, nj
 debug = 0
 
-# 2. 지도 만들기
-v = [v[lft:rgt+1] for v in v[up:dwn+1]]
-for i in range(len(v)):
-    for j in range(len(v[0])):
-        if not v[i][j] :
-            v[i][j] = '#'
-        else:
-            v[i][j] = '.'
+# [2] 출력
+for i in range(up, dwn+1):
+    print(*arr[i][lft:rgt+1], sep='')
 debug = 1
 
-# [2] 출력
-for vv in v :
-    print(*vv, sep='')
-debug = 2
